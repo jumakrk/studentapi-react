e334bea80885ff41eee636bbde82405273f69ebf
jumakrk <jumakrk@gmail.com>
2024-03-26 11:11:24 +0300

    made an add student form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Addstudentform from './components/Addstudentform'
+
+function App() {
+  return (
+    //call our components
+    <div className="App"> 
+    <Addstudentform/>
+    </div>
+  );
+}
+
+export default App;
